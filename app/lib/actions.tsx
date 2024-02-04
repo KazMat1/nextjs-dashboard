010b82becb7ae7d4ff17f1bb61d79301feeef54f
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2024-02-04 21:49:27 +0900

    [add] implement a update functionality

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -14,16 +14,15 @@ const FormSchema = z.object({
     status: z.enum(['pending', 'paid']),
     date: z.string(),
 });
-
-const CreateInvoice = FormSchema.omit({ id: true, date: true })
-
+const transformAmountToCents = (amount: number) => amount * 100;
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    const amountInCents = amount * 100;
+    const amountInCents = transformAmountToCents(amount);
     const date = new Date().toISOString().split('T')[0];
 
     await sql`
@@ -31,6 +30,26 @@ export async function createInvoice(formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     // revalidate a cache of index page because of creating a new invoice
+    revalidatePath(INVOICE_INDEX_PATH);
+    redirect(INVOICE_INDEX_PATH);
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = transformAmountToCents(amount);
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+
     revalidatePath(INVOICE_INDEX_PATH);
     redirect(INVOICE_INDEX_PATH);
 }
